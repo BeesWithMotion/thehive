6b0b4ea35e02ce50c618c08bd65f4c55e07d0951
ShaneWed <shanewedlock03@gmail.com>
2026-06-06 23:21:18 +0100

    Initial commit

---
+++ src/test/java/com/beeswithmotion/thehive/ThehiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beeswithmotion.thehive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThehiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
